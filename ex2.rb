27ba276926ccb4e14f9392bf82d22dc77e017599
Joe Letner <jrletner@gmail.com>
2023-01-12 18:15:28 -0600

    Update ex2.rb

--- ex2.rb
+++ ex2.rb
@@ -1,3 +1,13 @@
 num_arr = (1..100).to_a
 
-puts num_arr
+for n in num_arr.each
+  if n % 3 == 0 && n % 5 == 0
+    puts "FizzBuzz"
+  elsif n % 3 == 0
+    puts "Fizz"
+  elsif n % 5 == 0
+    puts "Buzz"
+  else
+    puts n
+  end
+end
