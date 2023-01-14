f46ece5f3f9d023ebf3438149764d9023a61f7bc
Joe Letner <jrletner@gmail.com>
2023-01-14 09:07:45 -0600

    Create class_ex2.rb

---
+++ class_ex2.rb
@@ -0,0 +1,5 @@
+# 1. Define a class call Person and set instance variables name and age using the method attr_accessors
+# 2. Define an initialize method that will set each attribute
+# 3. Create a class variable called people that is set to an empty array
+# 4. Every time an instance gets created, push that instance in the class variable people. Do so in the initialize method
+# 5. Create a class method called print_all_people. Loop through people and print each name and age in the the format of "Name: John Doe, Age: 54". Call this method to see if the results are corrects.
